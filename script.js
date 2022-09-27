7ab2b2a41d066a0aba3970572a54f78060ec39df
Sulic Octavian <octavius1992@gmail.com>
2022-09-27 12:42:21 +0300

    Add function to display books

--- script.js
+++ script.js
@@ -1,30 +1,49 @@
-let myLibrary = [];
-
 const bookName = document.getElementById('name').value;
 const bookAuthor = document.getElementById('author').value;
 const bookPages = document.getElementById('pages').value;
 const bookStatus = document.getElementById('status').value;
+const bookTable = document.getElementById('book-table');
 
+// Book storage
+let myLibrary = [];
+
+// Book constructor
 function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+  this.Title = title;
+  this.Author = author;
+  this.Pages = pages;
+  this.Status = read;
 }
-
-Book.prototype.info = function() {
-  return(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`);
-}
-
 const newBook = new Book(bookName, bookAuthor, bookPages, bookStatus);
 
-// Filling books
+// Book.prototype.info = function() {
+//   return(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`);
+// }
+
+// Dummy filling books
 myLibrary.push(new Book ('Invitation to a Beheading', 'Vladimir Nabokov', '223', 'Unread' ));
 myLibrary.push(new Book ('Pale Fire', 'Vladimir Nabokov', '246', 'Unread' ));
 
+// Add books to myLibrary
 function addBookToLibrary() {
   myLibrary.push(newBook);
 }
 
-// addBookToLibrary();
 console.log(myLibrary);
+
+// Display books in myLibrary
+function displayBooks() {
+  myLibrary.forEach(element => {
+    let card = document.createElement('div');
+    card.classList.add('card');
+    for (const property in element) {
+      let row = document.createElement('h3');
+      row.innerText = `${property}: ${element[property]}`
+      card.appendChild(row);
+      // console.log(element[property])
+    }
+    bookTable.appendChild(card);
+  });
+}
+
+displayBooks();
