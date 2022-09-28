4980b8656eb130258b932032c0774ac9cf7b8a0c
Sulic Octavian <octavius1992@gmail.com>
2022-09-28 14:20:51 +0300

    Add delete book button

--- script.js
+++ script.js
@@ -1,100 +1,153 @@
-const bookTable = document.getElementById('book-table');
-const display = document.getElementById('main');
+const booksDisplay = document.getElementById('book-table');
+const mainDisplay = document.getElementById('main');
+const table = document.createElement('table');
+const popupWindow = document.createElement('div')
+
 
 // Book storage
 let myLibrary = [];
 
 // Book constructor
-function Book(title, author, pages, read) {
+function Book(title, author, pages, status) {
   this.Title = title;
   this.Author = author;
   this.Pages = pages;
-  this.Status = read;
+  this.Status = status;
 }
 
 // Add books to myLibrary
 function addBookToLibrary() {
-  const bookName = document.getElementById('name').value;
+  const bookTitle = document.getElementById('title').value;
   const bookAuthor = document.getElementById('author').value;
   const bookPages = document.getElementById('pages').value;
-  const bookStatus = document.getElementById('status').value;
-  const newBook = new Book(bookName, bookAuthor, bookPages, bookStatus);
+  const bookStatus = document.getElementById('new-status').value;
+  const newBook = new Book(bookTitle, bookAuthor, bookPages, bookStatus);
   myLibrary.push(newBook);
 }
 
 // Dummy filling books
 myLibrary.push(new Book ('Invitation to a Beheading', 'Vladimir Nabokov', '223', 'Unread' ));
-myLibrary.push(new Book ('Pale Fire', 'Vladimir Nabokov', '246', 'Unread' ));
-
-console.log(myLibrary);
+myLibrary.push(new Book ('Pale Fire', 'Vladimir Nabokov', '246', 'Read' ));
 
 function displayBooks() {
-  const table = document.createElement('table');
-  let firstRow = document.createElement('tr');
-  let headName = document.createElement('th');
-  headName.innerText = 'Name';
-  firstRow.appendChild(headName);
+  // Create header row
+  let headRow = document.createElement('tr');
+  let headTitle = document.createElement('th');
+  headTitle.innerText = 'Title';
+  headRow.appendChild(headTitle);
   let headAuthor = document.createElement('th')
   headAuthor.innerText = 'Author';
-  firstRow.appendChild(headAuthor);
+  headRow.appendChild(headAuthor);
   let headPages = document.createElement('th')
   headPages.innerText = 'Pages';
-  firstRow.appendChild(headPages);
+  headRow.appendChild(headPages);
   let headStatus = document.createElement('th')
   headStatus.innerText = 'Status';
-  firstRow.appendChild(headStatus);
-  table.appendChild(firstRow);
+  headRow.appendChild(headStatus);
+  table.appendChild(headRow);
+  // Create row for each book
   myLibrary.forEach(book => {
-    let secondRow = document.createElement('tr');
+    let bookRow = document.createElement('tr');
+    bookRow.setAttribute('data-row', `row${myLibrary.indexOf(book)}`);
+    // Create cells for title/author/pages
     for (const property in book) {
       const cell = document.createElement('td');
+      cell.classList.add(`${property}`);
+      if (property !== 'Status') {
       const cellText = document.createTextNode(book[property])
       cell.appendChild(cellText);
-      secondRow.appendChild(cell);
+      }
+      // Create cells for status
+      if ( property === 'Status') {
+        const statusInput = document.createElement('select')
+        statusInput.setAttribute('name', 'status');
+        statusInput.setAttribute('id', 'status');
+        let statuses = ['Read', 'Unread', 'Ongoing']
+        statuses.forEach(item => {
+          let option = document.createElement('option');
+          option.value = item;
+          statusInput.appendChild(option);
+          option.innerText = item;
+          if (book[property] === item) {
+            option.setAttribute('selected', '')
+          }
+        })
+        // Delete book button
+        cell.appendChild(statusInput)
+        const deleteBook = document.createElement('button')
+        deleteBook.innerText = 'X';
+        deleteBook.addEventListener('click', deleteRow);
+        cell.appendChild(deleteBook);
+        function deleteRow(){
+          myLibrary.splice(myLibrary.indexOf(book), 1);
+          table.removeChild(bookRow);
+        }
+      }
+      bookRow.appendChild(cell);
     }
-    table.appendChild(secondRow);
+    table.appendChild(bookRow);
   });
-  bookTable.appendChild(table);
-  table.setAttribute("border", "2");
+  booksDisplay.appendChild(table);
+  // table.setAttribute("border", "2");
 }
 
+// Add new book button
 function addBookPopup() {
-  const window = document.createElement('div')
-  window.classList.add('new-window');
+  popupWindow.classList.add('new-window');
 
   const nameInput = document.createElement('input')
   nameInput.setAttribute('type', 'text');
-  nameInput.setAttribute('name', 'name');
-  nameInput.setAttribute('id', 'name');
-  nameInput.setAttribute('placeholder', 'Name');
-  window.appendChild(nameInput);
+  nameInput.setAttribute('name', 'title');
+  nameInput.setAttribute('id', 'title');
+  nameInput.setAttribute('placeholder', 'Title');
+  popupWindow.appendChild(nameInput);
 
   const authorInput = document.createElement('input')
   authorInput.setAttribute('type', 'text');
   authorInput.setAttribute('name', 'author');
   authorInput.setAttribute('id', 'author');
   authorInput.setAttribute('placeholder', 'Author');
-  window.appendChild(authorInput);
+  popupWindow.appendChild(authorInput);
 
   const pagesInput = document.createElement('input')
   pagesInput.setAttribute('type', 'number');
   pagesInput.setAttribute('name', 'pages');
   pagesInput.setAttribute('id', 'pages');
   pagesInput.setAttribute('placeholder', 'Pages');
-  window.appendChild(pagesInput);
+  popupWindow.appendChild(pagesInput);
 
-  const statusInput = document.createElement('input')
-  statusInput.setAttribute('type', 'radio');
-  statusInput.setAttribute('name', 'name');
-  statusInput.setAttribute('id', 'name');
-  statusInput.setAttribute('placeholder', 'Name');
-  window.appendChild(statusInput);
+  // Read/Unread/Ongoing button
+  const statusInput = document.createElement('select')
+  statusInput.setAttribute('name', 'status');
+  statusInput.setAttribute('id', 'new-status');
+  let statuses = ['Read', 'Unread', 'Ongoing']
+  statuses.forEach(item => {
+    let option = document.createElement('option');
+    option.value = item;
+    statusInput.appendChild(option);
+    option.innerText = item;
+  })
+  popupWindow.appendChild(statusInput);
+  
+  var submitButton = document.createElement('button')
+  submitButton.innerText = 'Submit';
+  submitButton.addEventListener('click', submitBook);
+  popupWindow.appendChild(submitButton);
+  
+  mainDisplay.appendChild(popupWindow);
+}
 
-  display.appendChild(window);
+function submitBook() {
+  addBookToLibrary();
+  table.innerHTML = ''
+  displayBooks();
+  popupWindow.innerHTML ='';
+  mainDisplay.removeChild(popupWindow);
 }
 
 const button = document.getElementById('new-book');
 button.addEventListener('click', addBookPopup)
 
-// displayBooks();
+displayBooks();
 // console.log('Testing bananas');
+
